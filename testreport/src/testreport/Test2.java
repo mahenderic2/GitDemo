3eefb178835a939064be612c49527e286a572792
mahender <mahenderic2@gmail.com>
2022-09-08 10:41:57 +0530

    first commit

---
+++ testreport/src/testreport/Test2.java
@@ -0,0 +1,12 @@
+package testreport;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String name3=	"kjkjdkfbkjjb";
+	String name4=	"jhvbkvkvkj";
+
+	}
+
+}
